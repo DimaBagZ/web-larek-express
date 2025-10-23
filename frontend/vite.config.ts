229521c965be846ad9c8b46394d4522d86a7daba
DimaBagZ <DimaBagZ@yandex.ru>
2025-10-24 00:38:49 +0300

    Fix image file names case sensitivity

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react(), tsconfigPaths({ root: __dirname })],
-  base: '/web-larek-express/',
+  // base: "/web-larek-express/", // Временно отключено для локальной разработки
   resolve: {
     alias: {
       $fonts: resolve("./src/vendor/fonts"),
