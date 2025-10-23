e6616c5739164c12c9e25f161ac8bf9a2a6c5228
DimaBagZ <DimaBagZ@yandex.ru>
2025-10-23 23:34:10 +0300

    Fix GitHub Pages paths with base configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react(), tsconfigPaths({ root: __dirname })],
+  base: '/web-larek-express/',
   resolve: {
     alias: {
       $fonts: resolve("./src/vendor/fonts"),
